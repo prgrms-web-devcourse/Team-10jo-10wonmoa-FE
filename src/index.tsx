c16a3de5c08d086fc3397deba24a4b236b062749
violet9503 <violet9503@gmail.com>
2022-07-25 04:17:49 +0900

    [#8] chore: createRoot 관련 경고메시지 해결

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createRoot } from 'react-dom/client';
 
 const queryClient = new QueryClient();
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const rootElement = document.getElementById('root') as HTMLElement;
+const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
