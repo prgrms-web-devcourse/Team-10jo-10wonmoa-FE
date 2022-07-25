e7f6ede21e9eb5caa68f33e5a56e94f656b90ede
sa02045 <sa02045@naver.com>
2022-07-25 21:35:29 +0900

    fix: 오타수정

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ const rootElement = document.getElementById('root') as HTMLElement;
 const root = createRoot(rootElement);
 
 if (process.env.NODE_ENV === 'development') {
-  const { worker } = require('./mocks/browers');
+  const { worker } = require('./mocks/browsers');
   worker.start();
 }
 
