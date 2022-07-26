045244fd9e71a847aac55585aa0ba4efed24626a
dahye1013 <alrean@naver.com>
2022-07-26 14:31:51 +0900

    chore: BaseComponent 추가 한 아이콘 및 서브타이틀 export 방식 추가

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,5 @@
 import AppLayout from './layout/AppLayout';
+import CoinIcon from './CoinIcon';
+import SubTitle from './SubTitle';
 
-export { AppLayout };
+export { AppLayout, CoinIcon, SubTitle };
