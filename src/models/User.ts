74aa1905140a929a0ab774112a32aad6ebb70c37
dahye1013 <alrean@naver.com>
2022-07-26 14:30:41 +0900

    feat: User 정보 기본 Model 추가

---
+++ src/models/User.ts
@@ -0,0 +1,13 @@
+class User {
+  email: string;
+  password: string;
+  username: string;
+
+  constructor() {
+    this.email = '';
+    this.password = '';
+    this.username = '';
+  }
+}
+
+export default User;
