b54deb673967c620bc5bf5eb95b5aa6a1c6fd2e6
dahye1013 <alrean@naver.com>
2022-07-30 21:06:37 +0900

    feat: 가계부 일일페이지 API 응답더미데이터 기반 props 세팅

--- src/pages/accountBookDaily/index.tsx
+++ src/pages/accountBookDaily/index.tsx
@@ -1,7 +1,74 @@
 import React from 'react';
 
-const AccountBookDaily = () => {
-  return <>AccountBookDaily</>;
+import { AccountBookDailyCard } from '@components/account';
+
+export type AccountType = 'INCOME' | 'EXPENDITURE';
+
+export type SingleAccount = {
+  id: string;
+  type: AccountType | unknown;
+  registerDate: string;
+  amount: number;
+  content: string;
+  categoryName: string;
+};
+
+const AccountBookDaily: React.FC = () => {
+  // API Response
+  const data = {
+    currentPage: 3,
+    nextPage: 4,
+    results: [
+      {
+        registerDate: '2022-07-20T22:11',
+        dayIncome: 30000,
+        dayExpenditure: 10000,
+        dayDetails: [
+          {
+            id: '111',
+            type: 'INCOME',
+            registerDate: '2022-07-20T22:11',
+            amount: 10000,
+            content: '오늘 얼마를 등록',
+            categoryName: '은행',
+          },
+          {
+            id: '111',
+            type: 'INCOME',
+            registerDate: '2022-07-20T22:11',
+            amount: 10000,
+            content: '오늘 얼마를 등록',
+            categoryName: '은행',
+          },
+        ],
+      },
+      {
+        registerDate: '2022-07-20T22:11',
+        dayIncome: 30000,
+        dayExpenditure: 10000,
+        dayDetails: [
+          {
+            id: '111',
+            type: 'INCOME',
+            registerDate: '2022-07-20T22:11',
+            amount: 10000,
+            content: '오늘 얼마를 등록',
+            categoryName: '은행',
+          },
+        ],
+      },
+    ],
+  };
+
+  const { results } = data;
+
+  return (
+    <>
+      {results.map((item) => (
+        <AccountBookDailyCard items={item} />
+      ))}
+    </>
+  );
 };
 
 export default AccountBookDaily;
