23ea442fc831b6533f9c07e8291aa07a1f9bca07
Jenet <95457808+hyunjin0910@users.noreply.github.com>
2022-08-02 02:21:45 +0900

    Feature/#74 StasticsItem 컴포넌트 (#82)
    * feat: StatisticItem 컴포넌트 구현
    
    * style: 폴더명 Statics -> Statistics로 변경
    
    * feat: currencyFormatter & 목업 데이터 임시 세팅
    
    * chore: import React
    
    * chore: eslint 수정

---
+++ src/pages/statistic/index.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import StatisticItem from '@components/statistic/StatisticItem';
+import { currencyFormatter } from '@utils/formatter/currencyFormatter';
+import { theme } from '@styles';
+import { BottomNavigation } from '@components';
+const Statistics = () => {
+  /**
+   * 임시 목업 데이터
+   * */
+  // const yearData = {
+  //   year: 2022,
+  //   incomes: [
+  //     {
+  //       name: '월급',
+  //       percent: 50,
+  //       total: 500000,
+  //     },
+  //     {
+  //       name: '주식',
+  //       percent: 30,
+  //       total: 300000,
+  //     },
+  //   ],
+  //   expenditures: [
+  //     {
+  //       name: '식비',
+  //       percent: 50,
+  //       total: 50000,
+  //     },
+  //     {
+  //       name: '패션/미용',
+  //       percent: 30,
+  //       total: 30000,
+  //     },
+  //     {
+  //       name: '교육',
+  //       percent: 20,
+  //       total: 20000,
+  //     },
+  //   ],
+  // };
+  const monthData = {
+    year: 2022,
+    month: 7,
+    incomes: [
+      {
+        name: '월급',
+        percent: 50,
+        total: 500000,
+      },
+      {
+        name: '주식',
+        percent: 30,
+        total: 300000,
+      },
+    ],
+    expenditures: [
+      {
+        name: '식비',
+        percent: 50,
+        total: 50000,
+      },
+      {
+        name: '패션/미용',
+        percent: 30,
+        total: 30000,
+      },
+      {
+        name: '교육',
+        percent: 20,
+        total: 20000,
+      },
+    ],
+  };
+  const colorList = ['red', 'orange', 'yellowgreen', 'green'];
+  // const { incomes, expenditures } = yearData;
+
+  const { expenditures } = monthData;
+  return (
+    <>
+      <ListWrapper>
+        {expenditures.map((item, idx) => (
+          <StatisticItem
+            key={idx}
+            percent={item.percent}
+            name={item.name}
+            total={currencyFormatter(item.total)}
+            color={colorList[idx]}
+          />
+        ))}
+      </ListWrapper>
+      <BottomNavigation />
+    </>
+  );
+};
+
+export default Statistics;
+
+const ListWrapper = styled.div`
+  width: 100%;
+  border-top: 2rem solid ${theme.$gray_light};
+`;
