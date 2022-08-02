290dd974c93fd0878d20eed9550340587651f101
Jenet <xuanchen0910@gmail.com>
2022-08-03 00:38:36 +0900

    feat: 밖의 영역 클릭 시 select 닫힘

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -2,27 +2,29 @@ import styled from '@emotion/styled';
 import { theme } from '@styles';
 import React, { useState } from 'react';
 import { ChevronDown } from 'react-feather';
+import { useClickAway } from '@hooks';
 interface DropDownInterface {
   type?: 'search' | 'statistics';
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
   //setDate?
 }
 
-const DropDown: React.FC<DropDownInterface> = ({
-  onClick,
-  type = 'statistics',
-}) => {
+const DropDown: React.FC<DropDownInterface> = ({ type = 'statistics' }) => {
   const [selectedMenu, setSelectedMenu] = useState('월별');
   const [showMenu, setShowMenu] = useState(false);
-  // false를 상단 페이지에서 상태를 내려줘야 함
   const handleShowMenu = () => setShowMenu(!showMenu);
+  const handleClickAway = () => {
+    if (showMenu) handleShowMenu();
+  };
+
+  const selectRef = useClickAway(handleClickAway);
   const handleSelectMenu = (e: React.MouseEvent) => {
     const eText = (e.target as HTMLElement).textContent;
     setSelectedMenu(eText || '');
     handleShowMenu();
   };
   return (
-    <MenuWrapper>
+    <MenuWrapper ref={selectRef}>
       <SelectedMenu onClick={handleShowMenu}>
         {selectedMenu} <ChevronDown />
       </SelectedMenu>
@@ -45,7 +47,10 @@ const MenuWrapper = styled.div`
   width: 5rem;
   border: 1px solid ${theme.$gray_dark};
   border-radius: 0.5rem;
+  background-color: ${theme.$white};
   padding: 0.2rem 0.6rem;
+  z-index: 10;
+  position: absolute;
 `;
 
 const SelectedMenu = styled.div`
