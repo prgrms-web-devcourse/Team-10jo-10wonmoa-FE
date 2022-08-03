daafeeda4f1d387d712a5e85f2cdd9637738b653
dahye1013 <alrean@naver.com>
2022-08-04 03:20:11 +0900

    feat: 가계부 daily api 호출

--- src/pages/accountBookDaily/index.tsx
+++ src/pages/accountBookDaily/index.tsx
@@ -1,117 +1,40 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styled from '@emotion/styled';
 import { useNavigate } from 'react-router-dom';
 import { AccountBookDailyCard, PlusButton } from '@components/account';
-import { GoTopButton } from '@components';
-import instance from '../../api/core';
-
-const ACCOUNT_TYPE = {
-  INCOME: 'INCOME',
-  EXPENDITURE: 'EXPENDITURE',
-} as const;
-
-export type SingleAccount = {
-  id: string;
-  type: typeof ACCOUNT_TYPE[keyof typeof ACCOUNT_TYPE];
-  registerDate: string;
-  amount: number;
-  content: string;
-  categoryName: string;
-};
+import { GoTopButton, Spinner, CoinIcon } from '@components';
+import useDailyAccount from '@hooks/account/useDailyAccount';
+import type { DailyAccount } from '@hooks/account/useDailyAccount';
 
 const AccountBookDaily: React.FC = () => {
+  const { dailyResult, isLoading } = useDailyAccount();
+  const { results } = dailyResult;
   const navigate = useNavigate();
-  console.log(process.env.REACT_APP_API_URL);
-
-  useEffect(() => {
-    const fetData = async () => {
-      const result = await instance.get('/account-book/2022-07-01');
-      console.log(result);
-    };
-    fetData();
-  }, []);
 
   // API Response
-  const data = {
-    currentPage: 3,
-    nextPage: 4,
-    results: [
-      {
-        registerDate: '2022-07-20T22:11',
-        dayIncome: 30000,
-        dayExpenditure: 10000,
-        dayDetails: [
-          {
-            id: '111',
-            type: ACCOUNT_TYPE.INCOME,
-            registerDate: '2022-07-20T22:11',
-            amount: 10000,
-            content: '오늘 얼마를 등록',
-            categoryName: '은행',
-          },
-          {
-            id: '222',
-            type: ACCOUNT_TYPE.INCOME,
-            registerDate: '2022-07-20T22:11',
-            amount: 10000,
-            content: '오늘 얼마를 등록',
-            categoryName: '은행',
-          },
-          {
-            id: '333',
-            type: ACCOUNT_TYPE.INCOME,
-            registerDate: '2022-07-20T22:11',
-            amount: 10000,
-            content: '오늘 얼마를 등록',
-            categoryName: '은행',
-          },
-          {
-            id: '444',
-            type: ACCOUNT_TYPE.INCOME,
-            registerDate: '2022-07-20T22:11',
-            amount: 10000,
-            content: '오늘 얼마를 등록',
-            categoryName: '은행',
-          },
-          {
-            id: '555',
-            type: ACCOUNT_TYPE.EXPENDITURE,
-            registerDate: '2022-07-20T22:11',
-            amount: 10000,
-            content: '오늘 얼마를 등록',
-            categoryName: '은행',
-          },
-        ],
-      },
-      {
-        registerDate: '2022-07-20T22:11',
-        dayIncome: 30000,
-        dayExpenditure: 10000,
-        dayDetails: [
-          {
-            id: '333',
-            type: ACCOUNT_TYPE.INCOME,
-            registerDate: '2022-07-20T22:11',
-            amount: 10000,
-            content: '오늘 얼마를 등록',
-            categoryName: '은행',
-          },
-        ],
-      },
-    ],
-  };
 
-  const { results } = data;
-
-  const handleNavigateCreateAccount = async (event: React.MouseEvent) => {
-    event.preventDefault();
-
+  const handleNavigateCreateAccount = async () => {
     navigate('/account/create');
   };
 
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (!isLoading && dailyResult.results === null) {
+    return (
+      <>
+        <p>👇 클릭해서 가계부를 등록해보세요!</p>
+        <a onClick={handleNavigateCreateAccount}>
+          <CoinIcon />
+        </a>
+      </>
+    );
+  }
+
   return (
     <CardArea>
-      {results.map((item, idx) => (
+      {results?.map((item: DailyAccount, idx: number) => (
         <AccountBookDailyCard key={idx} items={item} />
       ))}
       <GoTopButton />
