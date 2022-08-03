f72f6b6e0665a2b9bde99558bf99ec18e7a35781
dahye1013 <alrean@naver.com>
2022-08-03 15:10:59 +0900

    fix: 하드코딩 코드 제거

--- src/api/core/index.ts
+++ src/api/core/index.ts
@@ -1,20 +1,16 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL:
-    'http://ec2-3-34-252-245.ap-northeast-2.compute.amazonaws.com:8080/api/v1/',
-  // baseURL: process.env.REACT_APP_API_URL,
-  withCredentials: true,
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
-    Authorization:
-      'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJ0ZW53b25tb2EiLCJleHAiOjE2NTkzNDQ0MTksImlhdCI6MTY1OTM0MDgxOSwidXNlcklkIjozLCJlbWFpbCI6InRlc3RAdGVzdC5jb20ifQ.eee2xl1U7HYJ5nDtE5_6m5XKkFP5Hc3f5TjpNLcBOqrk27p1BjVQuaXK3fyVZ-85uos87wS4CcJu5rOpT3xWZw',
-    'Content-Type': 'application/json',
+    Authorization: '',
   },
 });
 
 instance.interceptors.response.use(
   (response) => response,
   (error) => {
+    // TODO: QueryClient를 통해 실제 에러 핸들링 처리
     if (error.response.status === 404 || error.response.status === 400) {
       // window.location.href = '/';
     }
