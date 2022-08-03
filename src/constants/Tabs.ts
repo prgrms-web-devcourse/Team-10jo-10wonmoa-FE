bd33032d1faa89a1ffd42c17c9094c5b304b0aa8
sa02045 <sa02045@naver.com>
2022-08-04 01:49:12 +0900

    feat: TABS 상수 추가 및 constants alias

---
+++ src/constants/Tabs.ts
@@ -0,0 +1,14 @@
+import type { Tabs } from '@types';
+
+const STATISTICS_TABS: Tabs = [
+  {
+    value: 'income',
+    title: '수입',
+  },
+  {
+    value: 'expenditure',
+    title: '지출',
+  },
+];
+
+export { STATISTICS_TABS };
