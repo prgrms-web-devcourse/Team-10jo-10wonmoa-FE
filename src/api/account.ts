0b09ed1e05a2c67c40fb40076d2e5d28c6ae409c
violet9503 <violet9503@gmail.com>
2022-08-03 16:25:00 +0900

    feat: 수입, 지출 수정, 삭제, 상세조회 api 함수 생성

--- src/api/account.ts
+++ src/api/account.ts
@@ -9,7 +9,30 @@ const fetchPostExpenditures = (createAccountForm: CreateAccountForm) =>
     .post('/expenditures', createAccountForm)
     .then((response) => response.data);
 
-const fetchGetIncomes = (id: string) => axios.get(`/incomes/${id}`);
+const fetchGetIncomes = (id: string) =>
+  axios.get(`/incomes/${id}`).then((response) => response.data);
+
+const fetchGetExpenditures = (id: string) =>
+  axios.get(`/expenditures/${id}`).then((response) => response.data);
+
+const fetchUpdateIncomes = (id: string, createAccountForm: CreateAccountForm) =>
+  axios
+    .put(`/incomes/${id}`, createAccountForm)
+    .then((response) => response.data);
+
+const fetchUpdateExpenditures = (
+  id: string,
+  createAccountForm: CreateAccountForm
+) =>
+  axios
+    .put(`/expenditures/${id}`, createAccountForm)
+    .then((response) => response.data);
+
+const fetchDeleteIncomes = (id: string) =>
+  axios.delete(`/incomes/${id}`).then((response) => response.data);
+
+const fetchDeleteExpenditures = (id: string) =>
+  axios.delete(`/expenditures/${id}`).then((response) => response.data);
 
 const fetchGetCategory = (kind: string) => {
   return axios
@@ -21,5 +44,10 @@ export {
   fetchPostIncomes,
   fetchPostExpenditures,
   fetchGetIncomes,
+  fetchGetExpenditures,
+  fetchUpdateIncomes,
+  fetchUpdateExpenditures,
+  fetchDeleteIncomes,
+  fetchDeleteExpenditures,
   fetchGetCategory,
 };
