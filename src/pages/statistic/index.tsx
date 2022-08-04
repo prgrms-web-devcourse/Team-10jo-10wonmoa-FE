766d75e2e9fbaa1c1c87e8225d6080ad121c0885
Devjo <50866506+sa02045@users.noreply.github.com>
2022-08-04 17:00:23 +0900

    Feature/#105 statics tab (#106)
    * feat: Tab 타입 추가
    
    * feat: TABS 상수 추가 및 constants alias
    
    * feat: Tabs 컴포넌트에서 자식 컴포넌트를 렌더링하도록
    
    * feat: Tabs 컴포넌트
    
    * fix: 코드, 주석 정리
    
    * design: TabsWrapper 높이 수정

--- src/pages/statistic/index.tsx
+++ src/pages/statistic/index.tsx
@@ -3,8 +3,16 @@ import styled from '@emotion/styled';
 import StatisticItem from '@components/statistic/StatisticItem';
 import { currencyFormatter } from '@utils/formatter';
 import { theme } from '@styles';
-import { BottomNavigation, DropDown, TopNavMonthSelector } from '@components';
+import {
+  BottomNavigation,
+  DropDown,
+  TopNavMonthSelector,
+  Tabs,
+} from '@components';
 import { useMonthSelector } from '@hooks';
+
+import type { TabItem } from '@components/Tabs';
+import { STATISTICS_TABS } from '../../constants/Tabs';
 
 const Statistics = () => {
   const {
@@ -90,6 +98,12 @@ const Statistics = () => {
 
   const { expenditures } = monthData;
 
+  const [currentTab, setCurrentTab] = useState<TabItem>(STATISTICS_TABS[0]);
+
+  const handleTabClick = (clickedTab: TabItem) => {
+    setCurrentTab(clickedTab);
+  };
+
   return (
     <>
       {isMonth ? (
@@ -116,7 +130,35 @@ const Statistics = () => {
         <button onClick={() => setIsMonth(false)}>YEAR</button>
         <button onClick={() => setIsMonth(true)}>MONTH</button>
       </div>
+
+      <TabsWrapper>
+        <Tabs tabItems={STATISTICS_TABS} onClick={handleTabClick}>
+          <ChartContainer>
+            {currentTab.title === '수입' && <div>수입차트</div>}
+            {currentTab.title === '지출' && <h1>지출차트</h1>}
+          </ChartContainer>
+        </Tabs>
+      </TabsWrapper>
+
       <ListWrapper>
+        {expenditures.map((item, idx) => (
+          <StatisticItem
+            key={idx}
+            percent={item.percent}
+            name={item.name}
+            total={currencyFormatter(item.total)}
+            color={colorList[idx]}
+          />
+        ))}
+        {expenditures.map((item, idx) => (
+          <StatisticItem
+            key={idx}
+            percent={item.percent}
+            name={item.name}
+            total={currencyFormatter(item.total)}
+            color={colorList[idx]}
+          />
+        ))}
         {expenditures.map((item, idx) => (
           <StatisticItem
             key={idx}
@@ -136,5 +178,20 @@ export default Statistics;
 
 const ListWrapper = styled.div`
   width: 100%;
+  height: 40rem;
+  overflow-y: scroll;
   border-top: 2rem solid ${theme.$gray_light};
+`;
+
+const TabsWrapper = styled.div`
+  width: 100%;
+  height: 30rem;
+`;
+
+const ChartContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
