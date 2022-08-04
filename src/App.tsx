f7be379a22c0c0419238770c4fc25b95f522e039
sa02045 <sa02045@naver.com>
2022-08-04 17:36:57 +0900

    fix: queryClient는 errorHandler를 주입받아 생성한다

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { Global, ThemeProvider } from '@emotion/react';
 import { theme, reset } from '@styles';
 import { AppLayout, Loading } from '@components';
-import { QueryClientProvider } from 'react-query';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import { queryClient } from '@api/react-query/queryClient';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import AppRouter from '@router';
-
+import useApiError from '@hooks/useApiError';
 const App = () => {
+  const { handleError } = useApiError();
+
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClient(handleError)}>
       <ThemeProvider theme={theme}>
         <Global styles={reset} />
         <AppLayout>
