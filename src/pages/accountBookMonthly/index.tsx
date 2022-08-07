9cf6aedf8269342e452987b12b0c0ae18943d97c
dahye1013 <alrean@naver.com>
2022-08-08 08:13:30 +0900

    feat: 월 시작-완료일 formatter 함수 추가

--- src/pages/accountBookMonthly/index.tsx
+++ src/pages/accountBookMonthly/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { currencyFormatter } from '@utils/formatter';
-
 import { useAccountBookMonthly } from '@hooks/account';
+import dayjs from 'dayjs';
+
 const AccountBookMonthly = () => {
   const { data } = useAccountBookMonthly();
 
@@ -12,16 +13,22 @@ const AccountBookMonthly = () => {
     return <p>등록 된 데이터가 없습니다.</p>;
   }
 
+  const monthRangeFormatter = (month: number) => {
+    const startOfMonth = dayjs().date(month).startOf('month').format('MM.DD');
+    const endOfMonth = dayjs().date(month).endOf('month').format('MM.DD');
+    return `${startOfMonth} ~ ${endOfMonth}`;
+  };
+
   return (
     <>
       {data.map((item: MonthlyAccount, idx: number) => (
         <Card key={idx}>
           <div>
             <p>{item.month}월</p>
-            <p>8.1~8.30</p>
+            <p>{monthRangeFormatter(item.month)}</p>
           </div>
           <div>{currencyFormatter(item.incomeSum)}</div>
-          <div>-{currencyFormatter(item.expenditureSum)}</div>
+          <div>{currencyFormatter(item.expenditureSum)}</div>
         </Card>
       ))}
     </>
