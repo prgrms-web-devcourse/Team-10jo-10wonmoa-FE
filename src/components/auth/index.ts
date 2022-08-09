e7606021e2d8183a9de295e312ef9dd5f7ffb520
dahye1013 <alrean@naver.com>
2022-08-09 19:10:06 +0900

    feat: auth 해당하는 컴포넌트 index.ts 모듈 import

---
+++ src/components/auth/index.ts
@@ -0,0 +1,7 @@
+import AuthFormWrapper from './AuthFormWrapper';
+import ErrorParagraph from './ErrorParagraph';
+import Title from './Title';
+import SubTitle from './SubTitle';
+import TitleSection from './TitleSection';
+
+export { AuthFormWrapper, ErrorParagraph, Title, SubTitle, TitleSection };
