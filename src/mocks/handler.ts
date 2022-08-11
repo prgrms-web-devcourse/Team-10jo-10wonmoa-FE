39492f36a7ffb7756577ccbc9993eeaab7e1aff6
sa02045 <sa02045@naver.com>
2022-08-11 20:22:58 +0900

    feat: 예산 모킹 핸들러 추가

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -2,12 +2,13 @@ import loginHandlers from './handlers/Users';
 import incomesHandlers from './handlers/Incomes';
 import expenditures from './handlers/Expenditures';
 import categoryHandlers from './handlers/Category';
-
+import budgetHandlers from './handlers/Budget';
 const handlers = [
   ...loginHandlers,
   ...incomesHandlers,
   ...expenditures,
   ...categoryHandlers,
+  ...budgetHandlers,
 ];
 
 export default handlers;
