eb932fcbd737a9b027b1654927b3cfd3c8188be1
dahye1013 <alrean@naver.com>
2022-08-13 11:01:31 +0900

    remove: 미사용 함수 제거

--- src/api/core/index.ts
+++ src/api/core/index.ts
@@ -2,44 +2,6 @@ import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 import tokenStorage from '@utils/storage/TokenStorage';
 import { fetchAccessToken } from '@api/auth';
 
-const getJWTHeader = (): Record<string, string> => {
-  const token = tokenStorage.getAccessToken();
-
-  return token
-    ? { Authorization: `Bearer ${tokenStorage.getAccessToken()}` }
-    : {};
-};
-
-const setInterceptors = (instance: AxiosInstance, isAuthRequire?: boolean) => {
-  instance.interceptors.request.use((config) => {
-    if (!isAuthRequire) return config;
-
-    return {
-      ...config,
-      headers: {
-        ...getJWTHeader(),
-      },
-    };
-  });
-
-  instance.interceptors.response.use(
-    (response) => response,
-    (error) => Promise.reject(error)
-  );
-
-  return instance;
-};
-
-const createInstance = (isAuthRequire: boolean) => {
-  const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
-  });
-  return setInterceptors(instance, isAuthRequire);
-};
-
-export const axiosInstance = createInstance(false);
-export const axiosAuthInstance = createInstance(true);
-
 const Instance = (
   baseURL = process.env.REACT_APP_API_URL,
   options?: AxiosRequestConfig
