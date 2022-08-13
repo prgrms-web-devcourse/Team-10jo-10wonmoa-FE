3adac20a652eba3b9e26af21b61a24f2ed368bab
Jenet <xuanchen0910@gmail.com>
2022-08-13 23:10:32 +0900

    design: dropdown 스타일 수정. 메뉴 border에 같이 감싸기

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -5,15 +5,11 @@ import { ChevronDown } from 'react-feather';
 import { useClickAway } from '@hooks';
 interface DropDownInterface {
   type?: 'search' | 'statistics';
-  top?: number;
-  right?: number;
   setIsMonth: Dispatch<SetStateAction<boolean>>;
 }
 
 const DropDown: React.FC<DropDownInterface> = ({
   type = 'statistics',
-  top,
-  right,
   setIsMonth,
 }) => {
   const [selectedMenu, setSelectedMenu] = useState('월별');
@@ -31,12 +27,18 @@ const DropDown: React.FC<DropDownInterface> = ({
     eText === '월별' ? setIsMonth(true) : setIsMonth(false);
   };
   return (
-    <MenuWrapper top={top} right={right} ref={selectRef}>
+    <MenuWrapper
+      ref={selectRef}
+      style={{
+        height: showMenu ? '6rem' : '2rem',
+        marginTop: showMenu ? '4rem' : '',
+      }}
+    >
       <SelectedMenu onClick={handleShowMenu}>
         {selectedMenu} <ChevronDown />
       </SelectedMenu>
       {showMenu ? (
-        <div className="월별">
+        <Menus>
           <Menu
             style={{
               color: selectedMenu === '월별' ? `${theme.$primary}` : '',
@@ -63,7 +65,7 @@ const DropDown: React.FC<DropDownInterface> = ({
               기간
             </Menu>
           )}
-        </div>
+        </Menus>
       ) : null}
     </MenuWrapper>
   );
@@ -71,15 +73,12 @@ const DropDown: React.FC<DropDownInterface> = ({
 
 export default DropDown;
 
-const MenuWrapper = styled.div<{ top?: number; right?: number }>`
+const MenuWrapper = styled.div`
   width: 5rem;
-  height: 2rem;
   border: 1px solid ${(props) => props.theme.$gray_dark};
   border-radius: 0.5rem;
   background-color: ${(props) => props.theme.$white};
-  padding: 0.2rem 0.6rem;
-  top: ${(props) => props.top}rem;
-  right: ${(props) => props.right}rem;
+  padding: 0.2rem 0.4rem;
 `;
 
 const SelectedMenu = styled.div`
@@ -87,11 +86,14 @@ const SelectedMenu = styled.div`
   justify-content: space-between;
 `;
 
-const Menu = styled.div`
-  width: 100%;
+const Menus = styled.div`
   background-color: ${(props) => props.theme.$white};
-  padding: 0.2rem 0;
+  padding: 0.3rem 2rem 0.3rem 0rem;
+`;
+
+const Menu = styled.div`
   &:hover {
     color: ${(props) => props.theme.$primary};
   }
+  z-index: 10;
 `;
