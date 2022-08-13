2e2b7de6094887abd95dff10abf7256531880b45
violet9503 <violet9503@gmail.com>
2022-08-14 00:07:08 +0900

    fix: 금액 포매팅 함수 boolean 받는 방식으로 수정

--- src/utils/formatter/currencyFormatter.ts
+++ src/utils/formatter/currencyFormatter.ts
@@ -1,5 +1,5 @@
 const KRW_REGEXP = new RegExp(/\B(?=(\d{3})+(?!\d))/, 'g');
 
-export const currencyFormatter = (n: number, suffix = '') => {
-  return n.toString().replace(KRW_REGEXP, ',') + suffix;
+export const currencyFormatter = (n: number, suffix = false) => {
+  return n.toString().replace(KRW_REGEXP, ',') + (suffix ? '원' : '');
 };
