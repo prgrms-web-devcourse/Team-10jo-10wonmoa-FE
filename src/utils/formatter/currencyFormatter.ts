0e4cfcd1754d32e2e5d94ac224d43a32d2db2890
violet9503 <violet9503@gmail.com>
2022-08-13 22:34:04 +0900

    feat: 금액 포매팅 함수 suffix 옵셔널 parameter 추가

--- src/utils/formatter/currencyFormatter.ts
+++ src/utils/formatter/currencyFormatter.ts
@@ -1,5 +1,5 @@
 const KRW_REGEXP = new RegExp(/\B(?=(\d{3})+(?!\d))/, 'g');
 
-export const currencyFormatter = (n: number) => {
-  return n.toString().replace(KRW_REGEXP, ',');
+export const currencyFormatter = (n: number, suffix = '') => {
+  return n.toString().replace(KRW_REGEXP, ',') + suffix;
 };
