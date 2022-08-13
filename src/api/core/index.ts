23efa325ba78a86940cbf4adec73734d72165476
dahye1013 <alrean@naver.com>
2022-08-13 10:55:15 +0900

    feat: axios request 객체 추가

--- src/api/core/index.ts
+++ src/api/core/index.ts
@@ -1,5 +1,6 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 import tokenStorage from '@utils/storage/TokenStorage';
+import { fetchAccessToken } from '@api/auth';
 
 const getJWTHeader = (): Record<string, string> => {
   const token = tokenStorage.getAccessToken();
@@ -38,3 +39,58 @@ const createInstance = (isAuthRequire: boolean) => {
 
 export const axiosInstance = createInstance(false);
 export const axiosAuthInstance = createInstance(true);
+
+const Instance = (
+  baseURL = process.env.REACT_APP_API_URL,
+  options?: AxiosRequestConfig
+) => {
+  const instance = axios.create({
+    baseURL,
+    timeout: 5000,
+    ...options,
+  });
+  return instance;
+};
+
+type Request = () => AxiosInstance;
+
+export const request: Request = () => {
+  return Instance();
+};
+
+export const authRequest: Request = () => {
+  const instance = Instance();
+
+  instance.interceptors.request.use((config) => {
+    return {
+      ...config,
+      headers: {
+        Authorization: `Bearer ${tokenStorage.getAccessToken()}`,
+      },
+    };
+  });
+
+  instance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      const {
+        config,
+        response: { status, data },
+      } = error;
+
+      const isExpiredAccessToken =
+        status === 401 && data.messages[0] === '만료된 access-token 입니다.';
+
+      if (isExpiredAccessToken) {
+        const data = await fetchAccessToken();
+        tokenStorage.setAccessToken(data.accessToken);
+        tokenStorage.setRefreshToken(data.refreshToken);
+        return axios(config);
+      }
+
+      return Promise.reject(error);
+    }
+  );
+
+  return instance;
+};
