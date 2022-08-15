0a0d02fcb61c3c9961e46dcecf0c069e3fc2ebe2
Devjo <50866506+sa02045@users.noreply.github.com>
2022-08-15 18:48:21 +0900

    fix: eslint 안쓰는 변수

--- src/pages/logo/index.tsx
+++ src/pages/logo/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from '@emotion/styled';
 import { CoinIcon } from '@components';
