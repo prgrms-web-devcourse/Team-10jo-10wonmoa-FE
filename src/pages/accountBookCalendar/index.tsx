c1b57e47af44d3003ebfdd6eb3740f03afbce178
Jenet <xuanchen0910@gmail.com>
2022-08-15 11:14:10 +0900

    fix: 데이터 과도한 호출 줄이기, 달력 높이 수정

--- src/pages/accountBookCalendar/index.tsx
+++ src/pages/accountBookCalendar/index.tsx
@@ -72,25 +72,19 @@ const AccountBookCalendar = () => {
       setCurrentDate(date);
     }
   }, [searchParams, calendarRef]);
-  const [apiData, setAPIData] = useState<CalendarAccount[] | undefined>();
-  const [events, setEvents] = useState<CalendarEvents[] | undefined>();
 
+  const events = [];
   const { isLoading, data } = useCalendar(currentDate);
 
-  useEffect(() => {
-    if (!isLoading) {
-      setAPIData(data);
-    }
-    if (apiData !== undefined) {
-      setEvents(makeCalendarData(apiData));
-    }
-  }, [data, apiData]);
+  if (!isLoading && data !== undefined) {
+    events.push(...makeCalendarData(data));
+  }
 
   return (
     <CalendarWrapper>
       <Calendar
         isReadOnly={true}
-        height="45rem"
+        height="35rem"
         view="month"
         month={{
           isAlways6Weeks: false,
@@ -114,6 +108,7 @@ export default AccountBookCalendar;
 
 const CalendarWrapper = styled.div`
   width: 100%;
-  height: 45rem;
+  height: 35rem;
+  padding-bottom: 7rem;
   overflow-y: auto;
 `;
