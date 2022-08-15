c7693bff63a6daef7cd2a8560df4fbe6dd60b5b8
Devjo <50866506+sa02045@users.noreply.github.com>
2022-08-15 18:50:40 +0900

    feat: 맨처음 들어왔을때 보여줄 로고페이지 (#202)
    * feat: 맨처음 들어왔을때 보여줄 로고페이지
    
    * fix: eslint 안쓰는 변수

--- src/styles/reset.ts
+++ src/styles/reset.ts
@@ -1,5 +1,7 @@
 import { css } from '@emotion/react';
 import theme from './theme';
+import animation from './animation';
+
 const reset = css`
   html,
   body,
@@ -309,20 +311,7 @@ const reset = css`
     display: none;
   }
 
-  @keyframes fadeIn {
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 1;
-    }
-  }
-
-  .fadeIn {
-    animation: fadeIn 0.5s;
-    -moz-animation: fadeIn 0.5s; /* Firefox */
-    -webkit-animation: fadeIn 0.5s; /* Safari and Chrome */
-  }
+  ${animation}
 `;
 
 export default reset;
