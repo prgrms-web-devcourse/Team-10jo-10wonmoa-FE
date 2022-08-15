05b0221eef6d4b06c4d7c169d09e43f0a45bfd8a
violet9503 <violet9503@gmail.com>
2022-08-15 12:45:52 +0900

    fix: 함수 선언 방식 수정

--- src/index.tsx
+++ src/index.tsx
@@ -9,11 +9,11 @@ if (process.env.REACT_APP_MOCK_API) {
   const { worker } = require('./mocks/browsers');
   worker.start();
 }
-function setScreenSize() {
+const setScreenSize = () => {
   const vh = window.innerHeight * 0.01;
 
   document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
+};
 
 setScreenSize();
 
